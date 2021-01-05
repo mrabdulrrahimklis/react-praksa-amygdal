1860c74ed5cc3b462416379678ceb03f674d3428
Abdulrahim Klis <abdulrahim.klis@flipkod.com>
2021-01-05 22:12:28 +0100

    arch

---
+++ src/components/Privacy/Privacy.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Privacy = () => {
+    return(
+        <>
+            123
+        </>
+    );
+}
+
+export default Privacy;
---
+++ src/components/Sigunp/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Signup = () => {
+    return(
+        <>
+            123
+        </>
+    );
+}
+
+export default Signup;
